9928f1740556f4334f2067c1d3079e9079052b00
mrfolfox4 <arranger1044@aim.com>
2017-12-13 19:46:12 +0100

    WIP nlp preprocessing pipeline

--- mojito/newsgroups.py
+++ mojito/newsgroups.py
@@ -1,8 +1,21 @@
+from collections import Counter
+
 import numpy as np
 from lime.lime_text import LimeTextExplainer
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from blessings import Terminal
+
+import nltk
+from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import LancasterStemmer
+from nltk.stem import PorterStemmer
+from nltk.stem import SnowballStemmer
+from nltk.tokenize.texttiling import TextTilingTokenizer
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
 
 from . import Problem, load, dump
 
@@ -15,6 +28,7 @@ class NewsgroupsProblem(Problem):
 
     Partially ripped from https://github.com/marcotcr/lime
     """
+
     def __init__(self, *args, labels=None, min_words=10, **kwargs):
         super().__init__(*args, **kwargs)
         self.min_words = min_words
@@ -38,6 +52,7 @@ class NewsgroupsProblem(Problem):
             print('caching preprocessed dataset...')
             dump(path, (dataset, all_pp_documents))
 
+        print('\n\nPROCESSED doc\n{}\n\n'.format(all_pp_documents[:20]))
         self.class_names = dataset.target_names
         if labels is None:
             self.labels = list(range(len(self.class_names)))
@@ -57,10 +72,41 @@ class NewsgroupsProblem(Problem):
         return model
 
     @staticmethod
-    def preprocess(data):
-        """Reduces documents to lists of adjectives, nouns, and verbs."""
-        import nltk
-        from nltk.stem.porter import PorterStemmer
+    def preprocess(data,
+                   tokenizer=None,
+                   stopwords=set(stopwords.words('english')),
+                   stemmer=None,
+                   filter_pos_tag=False,
+                   lemmatizer='wordnet',
+                   min_df=1, max_df=0.3):
+        """
+        Small NLP pipeline:
+        0. lower casing
+        0. tokenize
+        1. stopword removal
+        2. lemmatize/stemming
+        3. filtering by document frequency
+        4. pos tagging filtering
+        Reduces documents to lists of adjectives, nouns, and verbs.
+        """
+
+        n_docs = len(data)
+        if isinstance(min_df, float):
+            min_df = min_df * n_docs
+        if isinstance(max_df, float):
+            max_df = max_df * n_docs
+        print('Min doc freq {} and Max doc freq {} (all docs {})'.format(min_df, max_df, n_docs))
+
+        def get_stemmer(stemmer='snowball'):
+
+            if stemmer == 'porter':
+                return PorterStemmer()
+            elif stemmer == 'lancaster':
+                return LancasterStemmer()
+            elif stemmer == 'snowball':
+                return SnowballStemmer('english')
+            else:
+                raise ValueError('Unrecognized stemmer', stemmer)
 
         VALID_TAGS = set([
             'FW',   # Foreign word
@@ -70,7 +116,7 @@ class NewsgroupsProblem(Problem):
             'NN',   # Noun, singular or mass
             'NNS',  # Noun, plural
             'NNP',  # Proper noun, singular
-            'NNPS', # Proper noun, plural
+            'NNPS',  # Proper noun, plural
             'UH',   # Interjection
             'VB',   # Verb, base form
             'VBD',  # Verb, past tense
@@ -80,21 +126,48 @@ class NewsgroupsProblem(Problem):
             'VBZ',  # Verb, 3rd person singular present
         ])
 
-        stemmer = PorterStemmer()
+        # stemmer = PorterStemmer()
+        # processed_data = []
+        # for i, text in enumerate(data):
+        #     print('preprocessing document {} of {}'.format(i, len(data)), end='\t\r')
+        #     processed_text = ' '.join(stemmer.stem(token) for token, tag
+        #                               in nltk.pos_tag(nltk.word_tokenize(text))
+        #                               if tag in VALID_TAGS)
+        #     processed_data.append(processed_text)
+        # return processed_data
+
+        doc_freq = Counter()
         processed_data = []
         for i, text in enumerate(data):
-            print('preprocessing document {} of {}'.format(i, len(data)))
-            processed_text = ' '.join(stemmer.stem(token) for token, tag
-                                      in nltk.pos_tag(nltk.word_tokenize(text))
-                                      if tag in VALID_TAGS)
-            processed_data.append(processed_text)
+            print('preprocessing document {} of {}'.format(i, len(data)), end='\t\r')
+            # tokens = word_tokenize(text.lower())
+            if tokenizer is None:
+                tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(text.lower())
+            if stopwords:
+                tokens = [t for t in tokens if t not in stopwords]
+            if filter_pos_tag:
+                tokens = [t for t, tag in nltk.pos_tag(tokens) if tag in VALID_TAGS]
+            if lemmatizer is not None:
+                lemmatizer = WordNetLemmatizer()
+                tokens = [lemmatizer.lemmatize(t) for t in tokens]
+            elif stemmer is not None:
+                stemmer = get_stemmer(stemmer)
+                tokens = [stemmer.stem(t) for t in tokens]
+
+            for t in tokens:
+                doc_freq[t] += 1
+
+            tokens = [t for t in tokens if min_df <= doc_freq[t] <= max_df]
+            processed_data.append(' '.join(t for t in tokens))
+
         return processed_data
 
     def wrap_preproc(self, model):
         return model
 
     def explain(self, learner, train_examples, example, y,
-                num_samples=5000, num_features=10):
+                num_samples=5000, num_features=10, discretize=None):
         from sklearn.linear_model import Ridge
         from sklearn.pipeline import make_pipeline
 
@@ -120,32 +193,32 @@ class NewsgroupsProblem(Problem):
 
         for word, coeff in explanation.as_list():
             colored_word = _TERM.underline + \
-                           _TERM.bold + \
-                           (_TERM.red if coeff < 0 else _TERM.green) + \
-                           word + \
-                           _TERM.normal
+                _TERM.bold + \
+                (_TERM.red if coeff < 0 else _TERM.green) + \
+                word + \
+                _TERM.normal
             matches = list(re.compile(r'\b' + word + r'\b').finditer(text))
             matches.reverse()
             for match in matches:
                 start = match.start()
-                text = text[:start] + colored_word + text[start+len(word):]
+                text = text[:start] + colored_word + text[start + len(word):]
         return text
 
     def improve_explanation(self, example, y, explanation):
         class_name = _TERM.bold + \
-                     _TERM.color(y) + \
-                     self.class_names[y] + \
-                     _TERM.normal
+            _TERM.color(y) + \
+            self.class_names[y] + \
+            _TERM.normal
         document = self.highlight_words(self.full_documents[example], explanation)
 
         print(("The model thinks that this document is '{class_name}' " +
-              "because of the highlighted words:\n" +
-              "\n" +
-              "=" * 80 + "\n" +
-              "{document}\n" +
-              "\n" +
-              "=" * 80 + "\n" +
-              "The important words are:\n").format(**locals()))
+               "because of the highlighted words:\n" +
+               "\n" +
+               "=" * 80 + "\n" +
+               "{document}\n" +
+               "\n" +
+               "=" * 80 + "\n" +
+               "The important words are:\n").format(**locals()))
 
         for word, coeff in explanation.as_list():
             color = _TERM.red if coeff < 0 else _TERM.green
@@ -162,6 +235,6 @@ class NewsgroupsProblem(Problem):
         for true_word, true_coeff in true_explanation.as_list():
             for pred_word, pred_coeff in pred_explanation.as_list():
                 if true_word == pred_word and \
-                    np.sign(true_coeff) == np.sign(pred_coeff):
+                        np.sign(true_coeff) == np.sign(pred_coeff):
                     matches += 1
         return matches / len(true_explanation.as_list())
