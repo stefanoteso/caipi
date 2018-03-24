1f37454f94bd233863a359aea08e1f97ecfcb5e0
Stefano Teso <stefano.teso@gmail.com>
2018-03-24 18:39:57 +0100

    run: avoid LIME exception with too few examples.

--- run-caipi-ttt.sh
+++ run-caipi-ttt.sh
@@ -5,5 +5,5 @@ for S in random least-confident; do
 done
 
 for S in random least-confident; do
-    ./caipi.py ttt svm $S -p 0.01 -S 10000 -F 3 -S 15000 -E 0 -I
+    ./caipi.py ttt svm $S -p 0.01 -S 10000 -F 3 -S 15000 -E 10 -I
 done
