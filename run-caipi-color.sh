9763379c8d1897ad60a0eca8999034162429c699
Stefano Teso <stefano.teso@gmail.com>
2018-03-30 08:55:38 +0200

    Add run-caipi-colors.sh

---
+++ run-caipi-color.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+N_SAMPLES=2000 # make LIME stable enough
+
+for S in random least-confident; do
+    ./caipi.py colors-rule0 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4
+    ./caipi.py colors-rule1 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4
+done
+
+for S in random; do
+    ./caipi.py colors-rule0 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4 -E 0 -I
+    ./caipi.py colors-rule1 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4 -E 0 -I
+done
