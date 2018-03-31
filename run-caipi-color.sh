138fb74c0078a8c863652baea4ab26f888a1cfd0
Stefano Teso <stefano.teso@gmail.com>
2018-03-31 11:10:58 +0200

    Add toy and color running scripts.

--- run-caipi-color.sh
+++ run-caipi-color.sh
@@ -2,7 +2,7 @@
 
 N_SAMPLES=2000 # make LIME stable enough
 
-for S in random least-confident; do
+for S in random; do
     ./caipi.py colors-rule0 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4
     ./caipi.py colors-rule1 svm $S -T 70 -p 0.01 -S $N_SAMPLES -F 4
 done
---
+++ run-caipi-toy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for S in random; do
+    ./caipi.py toy svm $S -p 0.0001 -T 101 -e 1 -S 200 -F 3
+    ./caipi.py toy svm $S -p 0.0001 -T 101 -e 1 -S 200 -F 3 -E 0 -I
+done
