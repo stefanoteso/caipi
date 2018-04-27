cf830f4cae5921a909eba27a40709b074edbe0f7
Stefano Teso <stefano.teso@gmail.com>
2018-04-27 13:13:25 +0200

    learners: simplify.

--- caipi/learners.py
+++ caipi/learners.py
@@ -1,48 +1,101 @@
 import numpy as np
-from sklearn.svm import LinearSVC
-from sklearn.svm import SVC
-from sklearn.calibration import CalibratedClassifierCV
+from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import GridSearchCV
+from sklearn.gaussian_process import (GaussianProcessRegressor,
+                                      GaussianProcessClassifier)
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.utils import check_random_state
 
 from .utils import densify, vstack, hstack
 
 
-class SVMLearner:
-    def __init__(self, problem, strategy, C=1.0, kernel='linear', sparse=False,
-                 rng=None):
+class ActiveLearner:
+    def __init__(self, problem, rng=None):
         self.problem = problem
         self.rng = check_random_state(rng)
 
-        cv = StratifiedKFold(random_state=0)
-        if kernel != 'linear':
-            self._f_model = \
-            self._p_model = SVC(C=C,
-                                kernel=kernel,
-                                probability=True,
-                                decision_function_shape='ovr',
-                                random_state=0)
-        else:
-            if not sparse:
-                self._f_model = LinearSVC(C=C,
-                                          penalty='l2',
-                                          loss='hinge',
-                                          multi_class='ovr',
-                                          random_state=0)
-            else:
-                self._f_model = LinearSVC(C=C,
-                                          penalty='l1',
-                                          loss='squared_hinge',
-                                          dual=False,
-                                          multi_class='ovr',
-                                          random_state=0)
-            self._p_model = CalibratedClassifierCV(self._f_model,
-                                                   method='sigmoid',
-                                                   cv=cv)
+    def _check_preprocessed(self, X):
+        if X.ndim != 2:
+            return self.problem.preproc(X)
+        return X
+
+    def select_model(self, X, y):
+        raise NotImplementedError()
+
+    def fit(self, X, y):
+        X = self._check_preprocessed(X)
+        self._decision_model.fit(X, y)
+        if self._decision_model is not self._prob_model:
+            self._prob_model.fit(X, y)
+
+    def get_params(self):
+        try:
+            return np.array(self._decision_model.coef_, copy=True)
+        except AttributeError:
+            return None
+
+    def decision_function(self, X):
+        X = self._check_preprocessed(X)
+        return self._decision_model.decision_function(X)
+
+    def score(self, X, y):
+        X = self._check_preprocessed(X)
+        return self._decision_model.score(X, y)
+
+    def predict(self, X):
+        X = self._check_preprocessed(X)
+        return self._decision_model.predict(X)
+
+    def predict_proba(self, X):
+        X = self._check_preprocessed(X)
+        return self._prob_model.predict_proba(X)
+
+
+class LinearLearner(ActiveLearner):
+    def __init__(self, *args, strategy='random', model='svm', C=1.0,
+                 sparse=False, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        pm = None
+        if model == 'lr':
+            # logistic regression
+            dm = pm = LogisticRegression(C=C,
+                                         penalty='l2',
+                                         multi_class='ovr',
+                                         random_state=0)
+
+        elif model == 'svm':
+            # linear SVM (dense)
+            dm = LinearSVC(C=C,
+                           penalty='l2',
+                           loss='hinge',
+                           multi_class='ovr',
+                           random_state=0)
+
+        elif model == 'l1svm':
+            # linear SVM (sparse)
+            dm = LinearSVC(C=C,
+                           penalty='l1',
+                           loss='squared_hinge',
+                           dual=False,
+                           multi_class='ovr',
+                           random_state=0)
+
+        elif model == 'polysvm':
+            dm = pm = SVC(C=C,
+                          kernel='poly',
+                          probability=True,
+                          decision_function_shape='ovr',
+                          random_state=0)
+
+        if pm is None:
+            cv = StratifiedKFold(random_state=0)
+            pm = CalibratedClassifierCV(dm, method='sigmoid', cv=cv)
+
+        self._decision_model = dm
+        self._prob_model = pm
 
         self.select_query = {
             'random': self._select_at_random,
@@ -72,121 +125,27 @@ class SVMLearner:
         return examples[np.argmin(diffs)]
 
     def select_model(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
         Cs = np.logspace(-3, 3, 7)
-        grid = GridSearchCV(estimator=self._f_model,
+
+        X = self._check_preprocessed(X)
+        grid = GridSearchCV(estimator=self._decision_model,
                             param_grid=dict(C=Cs),
                             scoring='f1_weighted',
                             n_jobs=-1)
         grid.fit(X, y)
         best_C = grid.best_estimator_.C
         print('SVM: setting C to', best_C)
-        self._f_model.set_params(C=best_C)
-
-    def fit(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        self._f_model.fit(X, y)
-        if self._f_model is not self._p_model:
-            self._p_model.fit(X, y)
-
-    def score(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._f_model.score(X, y)
-
-    def get_params(self):
-        try:
-            return np.array(self._f_model.coef_, copy=True)
-        except AttributeError:
-            return None
-
-    def decision_function(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._f_model.decision_function(X)
-
-    def predict(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._f_model.predict(X)
-
-    def predict_proba(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._p_model.predict_proba(X)
+        self._decision_model.set_params(C=best_C)
+        # XXX do we have to update the prob model here?
 
 
-class LRLearner:
-    def __init__(self, problem, strategy, C=1.0, kernel='linear', rng=None):
-        self.problem = problem
-        self.rng = check_random_state(rng)
 
-        self._model = LogisticRegression(C=C,
-                                         penalty='l2',
-                                         random_state=0)
+class GPLearner(ActiveLearner):
+    def __init__(self, *args, strategy='random', **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.select_query = {
-            'random': self._select_at_random,
-            'least-confident': self._select_least_confident,
-        }[strategy]
-
-    def select_model(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        Cs = np.logspace(-3, 3, 7)
-        grid = GridSearchCV(estimator=self._model,
-                            param_grid=dict(C=Cs),
-                            scoring='f1_weighted',
-                            n_jobs=-1)
-        grid.fit(X, y)
-        best_C = grid.best_estimator_.C
-        print('LR: setting C to', best_C)
-        self._model.set_params(C=best_C)
-
-    def _select_at_random(self, problem, examples):
-        return self.rng.choice(sorted(examples))
-
-    def _select_least_confident(self, problem, examples):
-        examples = sorted(examples)
-        margins = np.abs(self.decision_function(problem.X[examples]))
-        # NOTE margins has shape (n_examples,) or (n_examples, n_classes)
-        if margins.ndim == 2:
-            margins = margins.min(axis=1)
-        return examples[np.argmin(margins)]
-
-    def fit(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        self._model.fit(X, y)
-
-    def get_params(self):
-        return np.array(self._model.coef_, copy=True)
-
-    def decision_function(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._model.decision_function(X)
-
-    def predict(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._model.predict(X)
-
-    def predict_proba(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._model.predict_proba(X)
-
-
-class GPLearner:
-    def __init__(self, problem, strategy, rng=None):
-        self.problem = problem
-        self.rng = check_random_state(rng)
-
-        self._r_model = GaussianProcessRegressor(random_state=0)
-        self._c_model = GaussianProcessClassifier(random_state=0)
+        self._decision_model = GaussianProcessClassifier(random_state=0)
+        self._prob_model = GaussianProcessRegressor(random_state=0)
 
         self.select_query = {
             'random': self._select_at_random,
@@ -201,21 +160,5 @@ class GPLearner:
 
     def _select_most_variance(self, problem, examples):
         examples = sorted(examples)
-        _, std = self._r_model.predict(problem.X[examples], return_std=True)
+        _, std = self._prob_model.predict(problem.X[examples], return_std=True)
         return examples[np.argmax(std)]
-
-    def fit(self, X, y):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        self._r_model.fit(X, y)
-        self._c_model.fit(X, y)
-
-    def predict(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._c_model.predict(X)
-
-    def predict_proba(self, X):
-        if X.ndim != 2:
-            X = self.problem.preproc(X)
-        return self._c_model.predict_proba(X)
