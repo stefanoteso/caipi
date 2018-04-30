28a03d608f2f585f4390397d95154c59ac1f3db8
Stefano Teso <stefano.teso@gmail.com>
2018-04-30 15:10:23 +0200

    draw: plot the rule coefficients for colors.

--- caipi-draw-weights.py
+++ caipi-draw-weights.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from caipi import load
 
+plt.style.use('ggplot')
+
 data = load(sys.argv[2])
 
 def nrm(w):
@@ -16,14 +18,44 @@ try:
     n_iters = data.shape[1]
     vectors = data[0,:,:,:].reshape((n_iters, -1))
 except:
-    vectors = [data['w_train'], data['w_corr'], data['w_both']]
+    vectors = np.vstack([data['w_train'], data['w_corr'], data['w_both']])
 
-print(vectors.shape)
+if vectors.shape[1] == 300:
+    from numpy.linalg import lstsq
+
+    RULE0_COORDS = {(0, 4), (0, 20), (0, 24), (4, 20), (4, 24), (20, 24)}
+    RULE0_BASIS = np.array([1.0 if (i, j) in RULE0_COORDS else 0.0
+                            for i in range(5*5)
+                            for j in range(i+1, 5*5)])
+
+    RULE1_COORDS = {(1, 2), (1, 3), (2, 3)}
+    RULE1_BASIS = np.array([-1.0 if (i, j) in RULE1_COORDS else 0.0
+                            for i in range(5*5)
+                            for j in range(i+1, 5*5)])
+
+    A = np.array([RULE0_BASIS, RULE1_BASIS]).T
+    b = vectors.T
+
+    alpha, residuals, _, _ = lstsq(A, b)
+    # (n_rules, n_iters)
+
+    fig, ax = plt.subplots(1, 1)
+    x = np.arange(0, alpha.shape[1])
+    coeff_rule0 = alpha[0,:]
+    ax.plot(x, coeff_rule0, label='coeff. of rule 0', linewidth=2)
+    coeff_rule1 = alpha[1,:]
+    ax.plot(x, coeff_rule1, label='coeff. of rule 1', linewidth=2)
+    ax.plot(x, residuals, label='residual', linewidth=2, linestyle=':')
+
+    legend = ax.legend(loc='upper right',
+                       shadow=False)
+
+    fig.savefig(sys.argv[1] + '__coeff', bbox_inches='tight', pad_inches=0)
+    quit()
+
 
 fig = plt.figure(figsize=(50, len(vectors)))
 ax = fig.add_subplot(111)
+ax.set_axis_off()
 ax.matshow(nrm(vectors), cmap=plt.get_cmap('gray'))
-#for i, w in enumerate(vectors):
-#    ax = fig.add_subplot(len(vectors), 1, i + 1)
-#    ax.matshow(nrm(w), cmap=plt.get_cmap('gray'))
 fig.savefig(sys.argv[1], bbox_inches='tight', pad_inches=0)
