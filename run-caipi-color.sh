619c78b7bf122574862205c4bfb3c8a0dc969886
Stefano Teso <stefano.teso@gmail.com>
2020-05-29 14:22:32 +0200

    run: fix the script.

--- run-caipi-color.sh
+++ run-caipi-color.sh
@@ -2,14 +2,16 @@
 
 for L in l1svm svm lr; do
 
+    # No corrections
     for S in random least-confident; do
-        ./caipi.py colors-rule1 l1svm $S -k 3 -p 0 -P 5 -T 101 -e 20 -E -1 -F 4 -S 2000 -R 100
-        ./caipi.py colors-rule1 l1svm $S -k 3 -p 0 -P 5 -T 101 -e 20 -E -1 -F 3 -S 2000 -R 100
+        ./caipi.py colors-rule0 $L $S -k 10 -p 0 -P 5 -T 101 -e 20 -E -1 -F 4 -S 2000 -R 100
+        ./caipi.py colors-rule1 $L $S -k 10 -p 0 -P 5 -T 101 -e 20 -E -1 -F 3 -S 2000 -R 100
     done
 
+    # With corrections
     for S in random least-confident; do
-        ./caipi.py colors-rule1 l1svm $S -k 3 -p 0 -P 5 -T 101 -e 20 -E 0 -F 4 -S 2000 -R 100
-        ./caipi.py colors-rule1 l1svm $S -k 3 -p 0 -P 5 -T 101 -e 20 -E 0 -F 3 -S 2000 -R 100
+        ./caipi.py colors-rule0 $L $S -k 10 -p 0 -P 5 -T 101 -e 20 -E 0 -F 4 -S 2000 -R 100
+        ./caipi.py colors-rule1 $L $S -k 10 -p 0 -P 5 -T 101 -e 20 -E 0 -F 3 -S 2000 -R 100
     done
 
 done
